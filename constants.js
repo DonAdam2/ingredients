50e3c56363ca2a54d2313515840dd3d350ecaacd
Adam Morsi <33934746+DonAdam2@users.noreply.github.com>
2020-04-13 11:22:54 +0300

    Initial commit

---
+++ constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+	port: '3000',
+	devServer: 'http://localhost',
+	rootDirectory: 'src',
+	outputDirectory: 'dist',
+	jsSubDirectory: 'js/',
+	cssSubDirectory: 'css/',
+	metaInfo: {
+		title: 'Adam Morsi',
+		description: 'description',
+		url: 'https://example.com/',
+		keywords: 'add you keywords',
+	},
+};
